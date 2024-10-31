fd23bffdca447e7270efe96f75a2a3e7c0995792
BCM28 <saturncat28@gmail.com>
2024-10-30 22:29:26 -0400

    alot of stuff, not done yet!

---
+++ questions.js
@@ -0,0 +1,96 @@
+questions = 
+    [[//Unitary
+        [//4
+            "I want uniform decisions across the country, for smoother decision-making and in turn a better government.",
+        ],
+        [//3
+            "In crises, a unitary response is more agile, with swift decision-making.",
+        ],
+        [//2
+            "I want to promote national cohesion by avoiding regional disparities.",
+            "Centralized control reduces bureaucracy and simplifies administration.",
+        ],
+        [//1
+            "Facilitating the quick and efficient implementation of nationwide policies in important to my goals.",
+        ]],
+     
+     [//Federalism
+        [//4
+            "Allowing regions to address the specific needs of their populations is one of my priorities."
+        ],
+        [//3
+            "The diversity of governance in a federal system can enrich democracy by amplifying regional voices.",
+            "I want to distribute power across different levels of government, promoting local autonomy."
+        ],
+        [//2
+            "Federalism can prevent excessive concentration of power, ensuring a system of checks and balances."
+        ],
+        [//1
+            "We should foster policy experimentation, enabling innovation across states or provinces.",
+        ]],
+     
+     [//Internationalist-Nationalist
+        [//4
+            "A united world government would be beneficial to mankind.",
+            "We are first and foremost citizens of Earth.",
+            "In order for humanity to survive, we must get past having separate nations."
+        ],
+        [//3
+            "The United Nations needs more power.",
+            "The United Nations should have a military to enforce its resolutions.",
+            "Having independent nations with the risk of global conflict creates a large threat to humanity as a whole."
+        ],
+        [//2
+            "I support regional unions, such as the European Union.",
+            "Governments should be accountable to the international community.",
+            "Nations fighting among each other gets in the way of progress."
+        ],
+        [//1
+            "International trade is generally beneficial.",
+            "Alliances are important for protecting a nation",
+            "Nations should cooperate whenever it benefits them both"
+        ]],
+     
+    [//Security
+        [//4
+            "Security is essential for protecting individuals and communities from harm."
+        ],
+        [//3
+            "National security is crucial for maintaining a country's sovereignty.",
+            "A secure society fosters trust in institutions and governance."
+        ],
+        [//2
+            "I want to achieve a stable environment conducive to economic and social development with a strong state."
+        ],
+        [//1
+            "Effective security measures help prevent crime and violence."
+        ]],
+    [//Freedom
+        [//4
+            "Freedom is a fundamental human right that allows for personal autonomy."
+        ],
+        [//3
+            "Excessive restrictions on freedom can lead to oppression and abuse of power.",
+            "Individuals should express their thoughts and beliefs without fear."
+        ],
+        [//2
+            "Freedom fosters creativity and innovation in society."
+        ],
+        [//1
+            "Protecting freedom is essential for my form of governance."
+        ]],
+    [//Democracy
+        [//4
+            "Power should be derived from the consent of the governed."
+        ],
+        [//3
+            "Promotes pluralism, allowing diverse viewpoints to coexist, is important to my ideology.",
+            "Democracy ensures the protection of individual rights and freedoms."
+        ],
+        [//2
+            "Citizens should participate in decision-making through regular elections."
+        ],
+        [//1
+            "Protecting freedom of choice is essential for my form of governance."
+        ]],
+    ]
